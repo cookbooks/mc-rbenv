871b2ed4ceb082c61a833a9df1ad886ac6152abb
Stephen Lauck <stpehen.lauck@gmail.com>
2012-08-08 14:35:46 -0700

    moved rbenv settings to bashrc write bashrc instead of profile remove dir resource for rbenv since generated auto from git source bashrc

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,24 +13,17 @@ search(:users, "ruby:*") do |u|
   rbenv_user_dir = "/home/#{rbenv_user}"
   rubies = u['ruby']
   rbenv_dir = "#{rbenv_user_dir}/.rbenv"
-  
-  # directory rbenv_dir do
-  #     owner rbenv_user
-  #     action :create
-  #   end
-  
+
   git rbenv_dir do
     user rbenv_user
     repository "git://github.com/sstephenson/rbenv.git"
     action :sync
   end
  
-  # setup bash profile add ons
-  # TODO move to it's own provider maybe
-  cookbook_file "#{rbenv_user_dir}/.profile" do
+  cookbook_file "#{rbenv_user_dir}/.bashrc" do
     owner rbenv_user
     mode '0700'
-    source "profile"
+    source "bashrc"
     action :create
   end
   
@@ -62,28 +55,27 @@ search(:users, "ruby:*") do |u|
       user rbenv_user
       cwd rbenv_user_dir
       code <<-EOH
-      source .profile
       export HOME=#{rbenv_user_dir}
       export TMPDIR=#{rbenv_user_dir}
       export PREFIX=#{rbenv_user_dir}/.rbenv/versions/#{ruby}
       export CONFIGURE_OPTS='--with-opt-dir=/opt/local/lib'
-
       # ruby compile flags to link correctly for smartos
       export LDFLAGS="-R/opt/local/lib -L/opt/local/lib -L/opt/local/lib/"
+      
+      source .bashrc
+      
       # first check /modpkg/ruby if version exists
       # if true, copy ruby from NFS
       # else install
       # then copy new install to NFS
-      source .profile
       if [ "`rbenv versions | grep #{ruby}`" ];
         then echo "#{ruby} already installed";
       elif [ -f /modpkg/ruby/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz ];
-        then echo "copying ruby from modpkg..." && mkdir -p  $HOME/.rbenv/versions &&  \
+        then echo "copying ruby from modpkg NFS share..." && mkdir -p  $HOME/.rbenv/versions &&  \
         tar -xzf /modpkg/ruby/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz -C $HOME/.rbenv/versions
       else
         # make sure to create os/version folder for ruby
         [  -d /modpkg/ruby/smartos-base64-1.7.1/#{rbenv_user} ] || echo "creating pkg directory on nfs share..." && mkdir -p /modpkg/ruby/smartos-base64-1.7.1/#{rbenv_user}
-        source .profile
         echo "installing ruby from source..." && \
         rbenv install #{ruby} && echo "creating tar file" && cd .rbenv/versions/ && \
         mkdir -p /modpkg/ruby/smartos-base64-1.7.1/#{rbenv_user} && \
